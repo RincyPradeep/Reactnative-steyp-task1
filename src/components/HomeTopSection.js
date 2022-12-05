c3511192ef6c3bb911f37e3b658763175d330fbb
Rincy Pradeep <rincypradeep15@gmail.com>
2022-12-05 13:44:34 +0530

    assets folder changed

--- src/components/HomeTopSection.js
+++ src/components/HomeTopSection.js
@@ -2,8 +2,8 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import React from 'react'
 
-import Message from '../assets/icons/message_not_black.svg'
-import Bell from '../assets/icons/bell_not_black.svg'
+import Message from '../assets/icons/message_black.svg'
+import Bell from '../assets/icons/bell_black.svg'
 
 
 const HomeTopSection = ({navigation}) => {
@@ -11,11 +11,17 @@ const HomeTopSection = ({navigation}) => {
     <View style={styles.topContainer}>
       <Text style={styles.title}>Shopline</Text>
       <View style={styles.iconContainer}>
-        <TouchableOpacity onPress={()=>navigation.navigate('Message')}>
+        <TouchableOpacity onPress={()=>navigation.navigate('Message')} style={styles.icon}>
           <Message width={30} height={30} />
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>3</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>navigation.navigate('Notification')}>
+        <TouchableOpacity onPress={()=>navigation.navigate('Notification')} style={styles.icon}>
           <Bell width={30} height={30} />
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>8</Text>
+          </View>
         </TouchableOpacity>
       </View>
     </View>
@@ -44,5 +50,26 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: 80
+  },
+
+  icon:{
+    position: 'relative'
+  },
+
+  badge:{
+    backgroundColor:'#FF3B30',
+    width: 15,
+    height: 15,
+    borderRadius: 15/2,
+    zIndex: 10,
+    position: 'absolute',
+    right:0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  badgeText:{
+    color: '#fff',
+    fontSize: 10
   }
 })
